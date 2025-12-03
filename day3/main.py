e0085e1c35a132b6b0bb6cc5813aed6bb83838af
CICD Pipeline <ML-JP-Azure@nnlife.co.jp>
2025-12-03 15:05:52 +0900

    day3

--- day3/main.py
+++ day3/main.py
@@ -6,38 +6,44 @@ filename = "sample.txt" if len(sys.argv) == 1 else "input.txt"
 
 with open(filename) as file:
     content = file.read()
-
-# Split the content into two parts based on a double newline
-part1, part2 = content.split('\n\n', 1)
-# print(part1)
-# print(part2)
-
-# Process part1 into a list of lists, splitting each line by ':' and stripping whitespace
-g1 = [[item.strip() for item in line.strip().split(':')] for line in part1.split('\n')]
+part1 = content.split('\n')
+g1 = [[int(c) for c in line] for line in part1]
 # print(g1)
 
-base_cached_dict = defaultdict(int)
-
-for item in g1:
-    if item[0] not in base_cached_dict:
-        base_cached_dict[item[0]] = int(item[1])
-
-# Process part2 into a list of lists, splitting each line by whitespace or '->' and stripping whitespace
-g2 = [[item.strip() for item in re.split(r'\s+|->', line) if item.strip()] for line in part2.split('\n')]
-print(g2)
-
-
+def find_max_positions(arr):
+    first_max_idx = arr.index(max(arr[:-1]))
+    second_max_idx = first_max_idx + 1 + arr[first_max_idx+1:].index(max(arr[first_max_idx+1:]))
+    return arr[first_max_idx], arr[second_max_idx]
 
 def part1():
-    return "part1 result"
+    result = 0
+    for item in g1:
+        max1, max2 = find_max_positions(item)
+        # print(f"Max values: {max1*10+max2}")
+        result += max1*10 + max2
+    return result
 
 
 
+def find_max_positions_v2(arr):
+    n = len(arr)
+    max_indices = []
+    start = 0
+    for i in range(12):
+        end = n - (12 - i)
+        max_idx = arr.index(max(arr[start:end+1]), start, end+1)
+        max_indices.append(arr[max_idx])
+        start = max_idx + 1
+    # 12個の最大値を連結して数字にする
+    return int(''.join(map(str, max_indices)))
 
 def part2():
-    return "part2 result"
-
-
+    result = 0
+    for item in g1:
+        max1 = find_max_positions_v2(item)
+        # print(f"Max values: {max1}")
+        result += max1
+    return result
 
 
 print("Part one:", part1())
