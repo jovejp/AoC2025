6f26f56ae30a63cc2203705b6f5a3fe384e4c217
Jove Zhou <Jove Zhou>
2025-12-12 19:23:06 +0900

    good luck

--- day12/main.py
+++ day12/main.py
@@ -1,28 +1,21 @@
 import  sys
-from time import time
 
 filename = "sample.txt" if len(sys.argv) == 1 else "input.txt"
 
 with open(filename) as file:
-    content = file.read()
+    contents = file.read()
+all_blocks = contents.split('\n\n')
+g1 = all_blocks[:-1]
+g2 = all_blocks[-1]
 
-# Split the content into two parts based on a double newline
-part1 = content.split('\n')
-g1 = [[c for c in line] for line in part1]
-
-
-def part1():
-    return "part1 not implemented"
-
-
-
-def part2():
-    return "part2 not implemented"
-
-
-t = time()
-print("Part one:", part1())
-print(time() - t)
-t = time()
-print("Part two:", part2())
-print(time() - t)
+block_sizes = [line.count("#")  for line in g1]
+square_area = [ int(part.split(':')[0].split('x')[0]) * int(part.split(':')[0].split('x')[1]) for part in g2.splitlines()]
+combined_requirements = [[[index, int(num)] for index, num in enumerate(part.split(':')[1].split())] for part in g2.splitlines() ]
+combined_aras = [sum([(block_sizes[item[0]] * item[1]) for item in line]) for line in combined_requirements]
+# print(square_area)
+# print(combined_aras)
+# the solution is not accurate, but the result seems correct.
+# example used is still wrong.
+# no idea for good solution.
+result = sum(1 if square_area[x] > combined_aras[x] else 0 for x in range(len(square_area)))
+print(result)
